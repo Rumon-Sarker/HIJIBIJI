69f7f5cdc4623c82bd763c699542f3033c0da4ad
Rafiuzzaman Rion <rafiujjaman5683@gmail.com>
2024-09-25 22:17:12 +0600

    updated

---
+++ src/app/admin/postBlog/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostBlog = () => {
+    return (
+        <div>
+            <h1>this is postblog page</h1>
+        </div>
+    );
+};
+
+export default PostBlog;
