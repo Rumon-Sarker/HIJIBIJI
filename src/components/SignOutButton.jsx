0440bcd2df417eb3bf01b6920445c98402d7f2ad
Rafiuzzaman Rion <rafiujjaman5683@gmail.com>
2024-09-26 22:55:47 +0600

    sign in method added

--- src/components/SignOutButton.jsx
+++ src/components/SignOutButton.jsx
@@ -1,13 +1,15 @@
-'use client'
-import {signOut} from 'next-auth/react';
-import React from 'react';
+"use client";
+import { signOut } from "next-auth/react";
+import React from "react";
 
 const SignOutButton = () => {
-    return (
-        <div>
-            <button onClick={()=> signOut()} className='btn bg-main'>Logout</button>
-        </div>
-    );
+  return (
+    <div>
+      <button onClick={() => signOut()} className="btn bg-main">
+        Logout
+      </button>
+    </div>
+  );
 };
 
 export default SignOutButton;
