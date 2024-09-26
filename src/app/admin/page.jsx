b5cf98049fa987c40ddf6c6f5ad6f0b658689b84
Rafiuzzaman Rion <rafiujjaman5683@gmail.com>
2024-09-26 22:51:31 +0600

    sign in page added

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,14 +1,12 @@
-
-import React from 'react';
-import Dashboard from './dashboard/Dashboard'
+import React from "react";
+import Dashboard from "./dashboard/Dashboard";
 // import AdminHome from './../../components/AdminHome'
 const page = () => {
- 
-    return (
-        <div>
-         <Dashboard/>
-        </div>
-    );
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
 };
 
 export default page;
