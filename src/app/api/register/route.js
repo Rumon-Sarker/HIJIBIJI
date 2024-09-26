12bc10a2e5ab02e343e46b1812dbd3cbe45a7c49
Rafiuzzaman Rion <rafiujjaman5683@gmail.com>
2024-09-26 23:03:36 +0600

    posted user data to database successfully

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
 import { PrismaClient } from "@prisma/client";
 
+
 const prisma = new PrismaClient();
 
 export async function POST(request) {
