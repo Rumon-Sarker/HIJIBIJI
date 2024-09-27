0e681dbe999eaa41b8bb0768b035d78f8f6a5cc4
Rafiuzzaman Rion <rafiujjaman5683@gmail.com>
2024-09-27 18:17:35 +0600

    login page design added

--- src/app/api/auth/login/page.jsx
+++ src/app/api/auth/login/page.jsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
@@ -25,24 +26,51 @@ const LoginPage = () => {
 
   return (
     <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Login</button>
-      </form>
+      <div className="flex justify-center">
+        <div className="bg-base-100 shadow-xl md:mx-0 mx-5 p-10 rounded-md w-96">
+          <h1 className="text-main text-4xl my-5 uppercase text-center">
+            Login Here
+          </h1>
+          <form className="space-y-5" onSubmit={handleSubmit}>
+            <label className="form-control w-full max-w-xs">
+              <span className="label-text">Email</span>
+              <input
+                className="input input-bordered"
+                type="email"
+                placeholder="Type Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </label>
+            <label className="form-control w-full max-w-xs">
+              <span className="label-text">Password</span>
+              <input
+                className="input input-bordered"
+                type="password"
+                placeholder="Type Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </label>
+            <div className="flex justify-between">
+              <h1>
+                Don&apos;t have an account?{" "}
+                <Link className="text-main link-hover" href={"/api/auth/login"}>
+                  Register Now
+                </Link>
+              </h1>
+              <button
+                className="btn bg-main hover:border-2 hover:border-main hover:bg-transparent hover:text-black text-white"
+                type="submit"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- src/app/api/auth/register/page.jsx
+++ src/app/api/auth/register/page.jsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import Link from "next/link";
 
 const RegisterPage = () => {
   const [email, setEmail] = useState("");
@@ -20,8 +21,13 @@ const RegisterPage = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+   <div>
+     
+     <div className="flex justify-center">
+    
+      <div className="bg-base-100 shadow-xl md:mx-0 mx-5 p-10 rounded-md w-96">
+      <h1 className="text-main text-4xl my-5 uppercase text-center">Register Here</h1>
+      <form className="space-y-5" onSubmit={handleSubmit}>
         <label className="form-control w-full max-w-xs">
           <span className="label-text">Name</span>
           <input
@@ -55,14 +61,19 @@ const RegisterPage = () => {
             required
           />
         </label>
-        <button
-          className="btn bg-main hover:border-2 hover:border-main hover:text-black text-white"
+       <div className="flex justify-between">
+        <h1>Already have an account? <Link className="text-main link-hover" href={'/api/auth/login'}>Login Now</Link></h1>
+       <button
+          className="btn bg-main hover:border-2 hover:border-main hover:bg-transparent hover:text-black text-white"
           type="submit"
         >
           Register
         </button>
+       </div>
       </form>
+      </div>
     </div>
+   </div>
   );
 };
 
