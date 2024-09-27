26318730e70adedb6fcf3d4826d1139aeed3dd2f
Rafiuzzaman Rion <rafiujjaman5683@gmail.com>
2024-09-27 23:08:51 +0600

    job request page added

---
+++ src/app/admin/jobRequest/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const jobRequest = () => {
+    return (
+        <div>
+            this is job request page
+        </div>
+    );
+};
+
+export default jobRequest;
