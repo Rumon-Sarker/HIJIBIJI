ffa0c47e3e3be7084aa0e686da5e7eb601311e66
Rafiuzzaman Rion <rafiujjaman5683@gmail.com>
2024-09-27 22:55:12 +0600

    video posting route added

--- src/app/api/auth/register/page.jsx
+++ src/app/api/auth/register/page.jsx
@@ -21,59 +21,64 @@ const RegisterPage = () => {
   };
 
   return (
-   <div>
-     
-     <div className="flex justify-center">
-    
-      <div className="bg-base-100 shadow-xl md:mx-0 mx-5 p-10 rounded-md w-96">
-      <h1 className="text-main text-4xl my-5 uppercase text-center">Register Here</h1>
-      <form className="space-y-5" onSubmit={handleSubmit}>
-        <label className="form-control w-full max-w-xs">
-          <span className="label-text">Name</span>
-          <input
-            className="input input-bordered"
-            type="text"
-            placeholder="Type Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </label>
-        <label className="form-control w-full max-w-xs">
-        <span className="label-text">Email</span>
-          <input
-            className="input input-bordered"
-            type="email"
-            placeholder="Type Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        <label className="form-control w-full max-w-xs">
-        <span className="label-text">Password</span>
-          <input
-            className="input input-bordered"
-            type="password"
-            placeholder="Create a strong Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-       <div className="flex justify-between">
-        <h1>Already have an account? <Link className="text-main link-hover" href={'/api/auth/login'}>Login Now</Link></h1>
-       <button
-          className="btn bg-main hover:border-2 hover:border-main hover:bg-transparent hover:text-black text-white"
-          type="submit"
-        >
-          Register
-        </button>
-       </div>
-      </form>
+    <div>
+      <div className="flex justify-center">
+        <div className="bg-base-100 shadow-xl md:mx-0 mx-5 p-10 rounded-md w-96">
+          <h1 className="text-main text-4xl my-5 uppercase text-center">
+            Register Here
+          </h1>
+          <form className="space-y-5" onSubmit={handleSubmit}>
+            <label className="form-control w-full max-w-xs">
+              <span className="label-text">Name</span>
+              <input
+                className="input input-bordered"
+                type="text"
+                placeholder="Type Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </label>
+            <label className="form-control w-full max-w-xs">
+              <span className="label-text">Email</span>
+              <input
+                className="input input-bordered"
+                type="email"
+                placeholder="Type Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </label>
+            <label className="form-control w-full max-w-xs">
+              <span className="label-text">Password</span>
+              <input
+                className="input input-bordered"
+                type="password"
+                placeholder="Create a strong Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </label>
+            <div className="flex justify-between">
+              <h1>
+                Already have an account?{" "}
+                <Link className="text-main link-hover" href={"/api/auth/login"}>
+                  Login Now
+                </Link>
+              </h1>
+              <button
+                className="btn bg-main hover:border-2 hover:border-main hover:bg-transparent hover:text-black text-white"
+                type="submit"
+              >
+                Register
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
-   </div>
   );
 };
 
