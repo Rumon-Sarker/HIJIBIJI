bfebe19c3fb3b77360036870fde360f7c2242437
MD Rafiuzzaman <rafiujjaman5683@gmail.com>
2024-10-22 13:44:47 +0600

    package.json updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['res.cloudinary.com', 'i.ibb.co.com', 'img.daisyui.com'],
+    domains: ["res.cloudinary.com", "i.ibb.co.com", "img.daisyui.com"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
 export default nextConfig;
-
