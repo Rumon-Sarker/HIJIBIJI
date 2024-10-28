c3610c4612819e373a0dc2f75cd6b64f54091887
MD Rafiuzzaman <rafiujjaman5683@gmail.com>
2024-10-28 21:21:31 +0600

    again testing

--- src/controls/postToService/createService.js
+++ src/controls/postToService/createService.js
@@ -3,21 +3,25 @@ import { PrismaClient } from "@prisma/client";
 import { uploadImage } from "../uploadImage/uploadImage";
 import { uploadPDF } from "../uploadPdf/uploadpdf";
 import { revalidatePath } from "next/cache";
+
 const prisma = new PrismaClient();
+
 export const createService = async (formData) => {
   const name = formData.get("name");
   const scope = formData.get("scope");
   const description = formData.get("description");
   const imageFile = formData.get("image");
   const pdfFile = formData.get("pdf");
-  // const imageUrl = await uploadImage(imageFile);
+
+  const imageUrl = await uploadImage(imageFile);
   const pdfUrl = await uploadPDF(pdfFile);
-  const imageUrl = "hijibusy.com";
+
   const line1 = formData.get("line1");
   const line2 = formData.get("line2");
   const line3 = formData.get("line3");
+
   try {
-    await prisma.services.create({
+    const serviceResponse = await prisma.services.create({
       data: {
         name,
         line1,
@@ -29,9 +33,10 @@ export const createService = async (formData) => {
         image: imageUrl,
       },
     });
-    return { status: "Service created successfully" };
+    return { status: "Service created successfully", service: serviceResponse };
   } catch (error) {
-    return { error: "failed to created Service" };
+    console.error("Error creating service:", error);
+    return { error: error.message || "Failed to create service" };
   } finally {
     revalidatePath("/services");
   }
